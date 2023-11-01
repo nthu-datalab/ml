7621bbbf938a7d402a1f3ec89a96aa09e7f473bf
unknown <supermice0000@gmail.com>
2023-11-02 03:45:48 +0800

    lab11 added!

---
+++ labs/11-1_CNN/lab11_1_lib.py
@@ -0,0 +1,53 @@
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+def draw_timeline(timeline, title, min_width=0.5, annotate=False, save=False):
+    # Remove invalid entries (negative times, or empty steps) from the timelines
+    invalid_mask = np.logical_and(timeline['times'] > 0, timeline['steps'] != b'')[:,0]
+    steps = timeline['steps'][invalid_mask].numpy()
+    times = timeline['times'][invalid_mask].numpy()
+    values = timeline['values'][invalid_mask].numpy()
+
+    # Get a set of different steps, ordered by the first time they are encountered
+    step_ids, indices = np.stack(np.unique(steps, return_index=True))
+    step_ids = step_ids[np.argsort(indices)]
+
+    # Shift the starting time to 0 and compute the maximal time value
+    min_time = times[:,0].min()
+    times[:,0] = (times[:,0] - min_time)
+    end = max(min_width, (times[:,0]+times[:,1]).max() + 0.01)
+
+    cmap = mpl.cm.get_cmap("plasma")
+    plt.close()
+    fig, axs = plt.subplots(len(step_ids), sharex=True, gridspec_kw={'hspace': 0})
+    fig.suptitle(title)
+    fig.set_size_inches(17.0, len(step_ids))
+    plt.xlim(-0.01, end)
+
+    order = [b'Open', b'Read', b'Map', b'Train', b'Val', b'Test', b'Epoch']
+    step_ids = sorted(step_ids, key=lambda i: order.index(i))
+    
+    for i, step in enumerate(step_ids):
+        step_name = step.decode()
+        ax = axs[i]
+        ax.set_ylabel(step_name)
+        ax.set_ylim(0, 1)
+        ax.set_yticks([])
+        ax.set_xlabel("time (s)")
+        ax.set_xticklabels([])
+        ax.grid(which="both", axis="x", color="k", linestyle=":")
+
+        # Get timings and annotation for the given step
+        entries_mask = np.squeeze(steps==step)
+        serie = times[entries_mask] # np.unique(times[entries_mask], axis=0)
+        annotations = values[entries_mask]
+
+        ax.broken_barh(serie, (0, 1), color=cmap(i / len(step_ids)), linewidth=1, alpha=0.66)
+        if annotate:
+            for j, (start, width) in enumerate(serie):
+                annotation = "\n".join([f"{l}: {v}" for l,v in zip(("i", "e", "s"), annotations[j])])
+                ax.text(start + 0.001 + (0.001 * (j % 2)), 0.55 - (0.1 * (j % 2)), annotation,
+                        horizontalalignment='left', verticalalignment='center')
+    if save:
+        plt.savefig(title.lower().translate(str.maketrans(" ", "_")) + ".svg")
